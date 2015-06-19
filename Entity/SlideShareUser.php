3d816ee423527a609afcbb316845834ed7c5d994
Sandro Groganz <sandro@campaignchain.com>
2015-06-19 16:14:52 +0200

    initial commit

---
+++ Entity/SlideShareUser.php
@@ -0,0 +1,147 @@
+<?php
+/*
+ * This file is part of the CampaignChain package.
+ *
+ * (c) Sandro Groganz <sandro@campaignchain.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CampaignChain\Location\SlideShareBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="campaignchain_location_slideshare_user")
+ */
+class SlideShareUser
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity="CampaignChain\CoreBundle\Entity\Location")
+     */
+    protected $location;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     */
+    protected $identifier;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     */
+    protected $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $displayName;
+    
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set identifier
+     *
+     * @param string $identifier
+     * @return CitrixUser
+     */
+    public function setIdentifier($identifier)
+    {
+        $this->identifier = $identifier;
+
+        return $this;
+    }
+
+    /**
+     * Get identifier
+     *
+     * @return string 
+     */
+    public function getIdentifier()
+    {
+        return $this->identifier;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return SlideShareUser
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }    
+
+    /**
+     * Set displayName
+     *
+     * @param string $displayName
+     * @return SlideShareUser
+     */
+    public function setDisplayName($displayName)
+    {
+        $this->displayName = $displayName;
+        return $this;
+    }
+    /**
+     * Get displayName
+     *
+     * @return string 
+     */
+    public function getDisplayName()
+    {
+        return $this->displayName;
+    }
+    
+    /**
+     * Set location
+     *
+     * @param \CampaignChain\CoreBundle\Entity\Location $location
+     * @return CitrixUser
+     */
+    public function setLocation(\CampaignChain\CoreBundle\Entity\Location $location = null)
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    /**
+     * Get location
+     *
+     * @return \CampaignChain\CoreBundle\Entity\Location
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+}
